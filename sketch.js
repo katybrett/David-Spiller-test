acfc80f2e729eb46f17f1586165d06143c9485c1
katybrett <katybrett@live.co.uk>
2017-04-19 16:03:43 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+function setup() {
+  createCanvas(605,605)
+}
+
+function draw() {
+
+  noStroke();
+
+  textSize(120);
+text("N E V E R", 20, 260);
+fill(255, 255, 255);
+
+textSize(110);
+text("WE WILL", 20, 98);
+fill(255, 255, 255);
+  
+  textSize(110);
+text("GROW  SO", 20, 420);
+fill(255, 255, 255);
+
+textSize(105);
+text("OLD AGAIN", 20, 585);
+fill(255, 255, 255);
+
+{strokeWeight(20)
+stroke(245, 245, 220)
+ 
+line(5, 5, 5, 600)
+line(5, 5, 600, 5)
+line(600, 5, 600, 600)
+line(5, 600, 600, 600)
+
+
+
+{translate(mouseX, mouseY);
+  textSize(30);
+  noStroke();
+fill(random(0, 255), random(0, 255), random(0, 255));
+ text("EVER", 5, 5);
+  }
+
+}
+ 
+}
+
+  
+
+  {noStroke();
+  textSize(20);
+  translate(width/32, height/15);
+rotate(PI/2.0);
+text("AND", 1, 1);
+fill(255, 255, 255);}
+
+
+
+
+
+
+
+
+
+
